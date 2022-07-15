2002e52fd60cfbec5f7c17880271bdd8be7cb61d
Acolapsys <timur_sh@bk.ru>
2022-07-15 11:06:28 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-pannellum :src="tour" style="height: 500px"></vue-pannellum>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VuePannellum from "vue-pannellum";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    VuePannellum
+  },
+  data() {
+    return {
+      tour: {
+        default: {
+          firstScene: "sea",
+          hotSpotDebug: true // для  поиска координат точек на изображении
+        },
+        scenes: {
+          sea: {
+            title: "Mason Circle",
+            hfov: 110,
+            pitch: -3,
+            yaw: 117,
+            type: "equirectangular",
+            panorama:
+              "https://mir-s3-cdn-cf.behance.net/project_modules/fs/83832185109119.5e57149f02878.jpg",
+            hotSpots: [
+              {
+                pitch: -1.4,
+                yaw: 150,
+                type: "scene",
+                text: "Spring House or Dairy",
+                sceneId: "venezia"
+              }
+            ]
+          },
+          venezia: {
+            hfov: 110,
+            yaw: 5,
+            type: "equirectangular",
+            panorama:
+              "https://roadtovrlive-5ea0.kxcdn.com/wp-content/uploads/2014/09/Venice.Still001.jpeg",
+            hotSpots: [
+              {
+                pitch: -3,
+                yaw: -52,
+                type: "scene",
+                text: "Mason Circle",
+                sceneId: "sea"
+              }
+            ]
+          }
+        }
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
